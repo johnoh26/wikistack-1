e7cb20377232bdf6f6cdd203fd000e42635fb2a1
johnoh26 <john.oh26@gmail.com>
2020-03-12 17:31:44 -0400

    working on improving our model with hooks

--- models/index.js
+++ models/index.js
@@ -1,39 +1,53 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack', {
-    logging: false
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/wikistack", {
+  logging: false
 });
 
-const User = db.define('user', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            isEmail: true
-        }
+const User = db.define("user", {
+  // Q: async/await? Why (not)?
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
     }
+  }
 });
 
-const Page = db.define('page', {
-    title: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    slug: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    content: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    status: {
-        type: Sequelize.ENUM('open', 'closed')
-    }
+const Page = db.define("page", {
+  // Q: async/await? Why (not)?
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM("open", "closed")
+  }
 });
+
+function slugFunction(arg) {
+  if (arg.length) {
+    // generate a random string slug
+  } else {
+    // arg =
+  }
+}
+
+Page.beforeValidate = page => {
+  page.slug = slugFunction(page.title); // Q: async/await? Why (not)?
+};
 
 module.exports = {
   db,
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const addPage = require('../views/addPage');
 const { Page } = require("../models");
 const { addPage } = require("../views");
 
-router.post('/', async (req, res, next) => {
-
+router.post("/", async (req, res, next) => {
   // STUDENT ASSIGNMENT:
   // add definitions for `title` and `content`
 
   const page = new Page({
-    title: req.body.title[2],
-    content: req.body.title[3]
+    title: req.body.title,
+    content: req.body.content
   });
 
   // make sure we only redirect *after* our save is complete!
   // note: `.save` returns a promise.
   try {
     await page.save();
-    res.redirect('/');
-  } catch (error) { next(error) }
+    res.redirect("/");
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get('/', (req, res, next) => {
-    res.send('got to GET /wiki/')
+router.get("/", async (req, res, next) => {
+  await res.send("got to GET /wiki/");
 });
-router.post('/', (req, res, next) => {
-    res.json(req.body);
+router.post("/", async (req, res, next) => {
+  await res.json(req.body);
+  await res.send("got to POST /wiki/");
 });
-router.get('/add', async (req, res, next) => {
-   await res.send(addPage());
+router.get("/add", async (req, res, next) => {
+  await res.send(addPage());
 });
 
 module.exports = router;
-
